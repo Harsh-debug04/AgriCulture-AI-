4e8182b996748ca3d9f35accb053a4f5b56c0d82
Harshwardhan Pandey <harshpandey145@gmail.com>
2025-09-19 08:06:43 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,10 @@ import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  "projectId": "studio-3607344020-df78a",
-  "appId": "1:36825555311:web:82a6795f465bc1c6e4089a",
-  "apiKey": "AIzaSyB6dPfUkwEXadOv5R55qoEq9MAvYgKYOWk",
-  "authDomain": "studio-3607344020-df78a.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "36825555311"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
